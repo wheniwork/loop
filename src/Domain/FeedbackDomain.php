cb48bba867d74773aeff8b6eb656d156e08482ae
Ben Visness <bvisness@gmail.com>
2015-07-30 12:04:56 -0500

    Move Redis stuff to FeedbackGetDomain
    The only domains that need Redis are domains that use GET to retrieve
    their data, so all Redis-related methods have been moved from
    FeedbackDomain to FeedbackGetDomain.

--- src/Domain/FeedbackDomain.php
+++ src/Domain/FeedbackDomain.php
@@ -3,23 +3,15 @@ namespace Wheniwork\Feedback\Domain;
 
 use Spark\Adr\DomainInterface;
 use Aura\Payload\Payload;
-use Predis\Client as RedisClient;
 use Wheniwork\Feedback\Service\GithubService;
 use Wheniwork\Feedback\Service\HipChatService;
 
 abstract class FeedbackDomain implements DomainInterface
 {
-    protected $redis;
-
     const POSITIVE = 'POSITIVE';
     const PASSIVE = 'PASSIVE';
     const NEGATIVE = 'NEGATIVE';
     const NEUTRAL = 'NEUTRAL';
-
-    public function __construct(RedisClient $redis)
-    {
-        $this->redis = $redis;
-    }
 
     public function getPayload()
     {
@@ -49,29 +41,6 @@ abstract class FeedbackDomain implements DomainInterface
         HipChatService::postMessage("<strong>From $source:</strong> $body", $color);
 
         GithubService::createIssue("Feedback from $source", $body);
-    }
-
-    /**
-     * Gets the key for this domain's cache in Redis.
-     *
-     * @return string   The key to use in Redis.
-     */
-    abstract protected function getRedisKey();
-
-    /**
-     * Gets the cached value for this domain from Redis.
-     */
-    protected function getRedisValue() {
-        return $this->redis->get($this->getRedisKey());
-    }
-
-    /**
-     * Sets the Redis cache for this domain.
-     *
-     * @param int $value    The value to save in Redis.
-     */
-    protected function setRedisValue($value) {
-        $this->redis->set($this->getRedisKey(), $value);
     }
 
     /**
